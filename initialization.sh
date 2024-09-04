8d7d89d58b39a833ae644d557a055703beb6692c
An0nymiz <akiannasr@student.smusd.us>
2024-09-04 13:05:07 -0700

    Create initialization.sh

---
+++ initialization.sh
@@ -0,0 +1,7 @@
+echo "# Anon" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/An0nymiz/Anon.git
+git push -u origin main
